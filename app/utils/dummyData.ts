55ad52858b745ca12bd28f1b3085fb0dd5827ead
Mohammad Rad <argo.mohrad@gmail.com>
2024-08-29 17:00:01 -0700

    Add dummy financial data for calendar visualization testing

---
+++ app/utils/dummyData.ts
@@ -0,0 +1,658 @@
+export const dummyFinancialData = {
+    "income_tracking": {
+      "recorded_income": [
+        {
+          "amount": 25,
+          "date": "2024-08-22",
+          "name": "CREDIT CARD 3333 PAYMENT *//"
+        },
+        {
+          "amount": 5.4,
+          "date": "2024-08-22",
+          "name": "Uber 063015 SF**POOL**"
+        },
+        {
+          "amount": 5850,
+          "date": "2024-08-21",
+          "name": "ACH Electronic CreditGUSTO PAY 123456"
+        },
+        {
+          "amount": 1000,
+          "date": "2024-08-21",
+          "name": "CD DEPOSIT .INITIAL."
+        },
+        {
+          "amount": 78.5,
+          "date": "2024-08-20",
+          "name": "Touchstone Climbing"
+        },
+        {
+          "amount": 12,
+          "date": "2024-08-19",
+          "name": "McDonald's"
+        },
+        {
+          "amount": 4.33,
+          "date": "2024-08-19",
+          "name": "Starbucks"
+        },
+        {
+          "amount": 89.4,
+          "date": "2024-08-18",
+          "name": "SparkFun"
+        },
+        {
+          "amount": 500,
+          "date": "2024-08-07",
+          "name": "United Airlines"
+        },
+        {
+          "amount": 6.33,
+          "date": "2024-08-05",
+          "name": "Uber 072515 SF**POOL**"
+        },
+        {
+          "amount": 500,
+          "date": "2024-08-02",
+          "name": "Tectra Inc"
+        },
+        {
+          "amount": 2078.5,
+          "date": "2024-08-01",
+          "name": "AUTOMATIC PAYMENT - THANK"
+        },
+        {
+          "amount": 500,
+          "date": "2024-08-01",
+          "name": "KFC"
+        },
+        {
+          "amount": 500,
+          "date": "2024-08-01",
+          "name": "Madison Bicycle Shop"
+        },
+        {
+          "amount": 25,
+          "date": "2024-07-23",
+          "name": "CREDIT CARD 3333 PAYMENT *//"
+        },
+        {
+          "amount": 5.4,
+          "date": "2024-07-23",
+          "name": "Uber 063015 SF**POOL**"
+        },
+        {
+          "amount": 5850,
+          "date": "2024-07-22",
+          "name": "ACH Electronic CreditGUSTO PAY 123456"
+        },
+        {
+          "amount": 1000,
+          "date": "2024-07-22",
+          "name": "CD DEPOSIT .INITIAL."
+        },
+        {
+          "amount": 78.5,
+          "date": "2024-07-21",
+          "name": "Touchstone Climbing"
+        },
+        {
+          "amount": 12,
+          "date": "2024-07-20",
+          "name": "McDonald's"
+        },
+        {
+          "amount": 4.33,
+          "date": "2024-07-20",
+          "name": "Starbucks"
+        },
+        {
+          "amount": 89.4,
+          "date": "2024-07-19",
+          "name": "SparkFun"
+        },
+        {
+          "amount": 500,
+          "date": "2024-07-08",
+          "name": "United Airlines"
+        },
+        {
+          "amount": 6.33,
+          "date": "2024-07-06",
+          "name": "Uber 072515 SF**POOL**"
+        },
+        {
+          "amount": 500,
+          "date": "2024-07-03",
+          "name": "Tectra Inc"
+        },
+        {
+          "amount": 2078.5,
+          "date": "2024-07-02",
+          "name": "AUTOMATIC PAYMENT - THANK"
+        },
+        {
+          "amount": 500,
+          "date": "2024-07-02",
+          "name": "KFC"
+        },
+        {
+          "amount": 500,
+          "date": "2024-07-02",
+          "name": "Madison Bicycle Shop"
+        },
+        {
+          "amount": 25,
+          "date": "2024-06-23",
+          "name": "CREDIT CARD 3333 PAYMENT *//"
+        },
+        {
+          "amount": 5.4,
+          "date": "2024-06-23",
+          "name": "Uber 063015 SF**POOL**"
+        },
+        {
+          "amount": 5850,
+          "date": "2024-06-22",
+          "name": "ACH Electronic CreditGUSTO PAY 123456"
+        },
+        {
+          "amount": 1000,
+          "date": "2024-06-22",
+          "name": "CD DEPOSIT .INITIAL."
+        },
+        {
+          "amount": 78.5,
+          "date": "2024-06-21",
+          "name": "Touchstone Climbing"
+        },
+        {
+          "amount": 12,
+          "date": "2024-06-20",
+          "name": "McDonald's"
+        },
+        {
+          "amount": 4.33,
+          "date": "2024-06-20",
+          "name": "Starbucks"
+        },
+        {
+          "amount": 89.4,
+          "date": "2024-06-19",
+          "name": "SparkFun"
+        },
+        {
+          "amount": 500,
+          "date": "2024-06-08",
+          "name": "United Airlines"
+        },
+        {
+          "amount": 6.33,
+          "date": "2024-06-06",
+          "name": "Uber 072515 SF**POOL**"
+        },
+        {
+          "amount": 500,
+          "date": "2024-06-03",
+          "name": "Tectra Inc"
+        },
+        {
+          "amount": 2078.5,
+          "date": "2024-06-02",
+          "name": "AUTOMATIC PAYMENT - THANK"
+        },
+        {
+          "amount": 500,
+          "date": "2024-06-02",
+          "name": "KFC"
+        },
+        {
+          "amount": 500,
+          "date": "2024-06-02",
+          "name": "Madison Bicycle Shop"
+        }
+      ],
+      "expected_income": [
+        {
+          "amount": 796.3900000000003,
+          "date": "2024-07-02"
+        }
+      ]
+    },
+    "expense_tracking": {
+      "total_monthly_expenses": 504.22,
+      "top_spending_categories": [
+        [
+          "TRAVEL",
+          1500
+        ],
+        [
+          "INCOME",
+          12.66
+        ]
+      ],
+      "expense_trend": [
+        [
+          "2024-06",
+          504.22
+        ],
+        [
+          "2024-07",
+          504.22
+        ],
+        [
+          "2024-08",
+          504.22
+        ]
+      ]
+    },
+    "balance_overview": {
+      "current_balance": 195466.8005,
+      "average_daily_balance": 211434.6605
+    },
+    "budgeting": {
+      "monthly_budget": 5000,
+      "progress": 0.899156
+    },
+    "financial_planning": {
+      "monthly_savings_goal": 2229.892000000001,
+      "current_savings_rate": 0.9547762851295041,
+      "recommendations": [
+        "Try to save 20% of your income each month",
+        "Focus on reducing expenses in your top spending category",
+        "Build an emergency fund of 3-6 months of expenses"
+      ]
+    },
+    "calendar_visualization": [
+      {
+        "date": "2024-08-22",
+        "transactions": [
+          {
+            "amount": 25,
+            "name": "CREDIT CARD 3333 PAYMENT *//",
+            "category": "LOAN_PAYMENTS"
+          },
+          {
+            "amount": 5.4,
+            "name": "Uber 063015 SF**POOL**",
+            "category": "TRANSPORTATION"
+          }
+        ]
+      },
+      {
+        "date": "2024-08-21",
+        "transactions": [
+          {
+            "amount": 5850,
+            "name": "ACH Electronic CreditGUSTO PAY 123456",
+            "category": "GENERAL_SERVICES"
+          },
+          {
+            "amount": 1000,
+            "name": "CD DEPOSIT .INITIAL.",
+            "category": "GENERAL_MERCHANDISE"
+          }
+        ]
+      },
+      {
+        "date": "2024-08-20",
+        "transactions": [
+          {
+            "amount": 78.5,
+            "name": "Touchstone Climbing",
+            "category": "PERSONAL_CARE"
+          },
+          {
+            "amount": -500,
+            "name": "United Airlines",
+            "category": "TRAVEL"
+          }
+        ]
+      },
+      {
+        "date": "2024-08-19",
+        "transactions": [
+          {
+            "amount": 12,
+            "name": "McDonald's",
+            "category": "FOOD_AND_DRINK"
+          },
+          {
+            "amount": 4.33,
+            "name": "Starbucks",
+            "category": "FOOD_AND_DRINK"
+          }
+        ]
+      },
+      {
+        "date": "2024-08-18",
+        "transactions": [
+          {
+            "amount": 89.4,
+            "name": "SparkFun",
+            "category": "ENTERTAINMENT"
+          }
+        ]
+      },
+      {
+        "date": "2024-08-17",
+        "transactions": [
+          {
+            "amount": -4.22,
+            "name": "INTRST PYMNT",
+            "category": "INCOME"
+          }
+        ]
+      },
+      {
+        "date": "2024-08-07",
+        "transactions": [
+          {
+            "amount": 500,
+            "name": "United Airlines",
+            "category": "TRAVEL"
+          }
+        ]
+      },
+      {
+        "date": "2024-08-05",
+        "transactions": [
+          {
+            "amount": 6.33,
+            "name": "Uber 072515 SF**POOL**",
+            "category": "TRANSPORTATION"
+          }
+        ]
+      },
+      {
+        "date": "2024-08-02",
+        "transactions": [
+          {
+            "amount": 500,
+            "name": "Tectra Inc",
+            "category": "ENTERTAINMENT"
+          }
+        ]
+      },
+      {
+        "date": "2024-08-01",
+        "transactions": [
+          {
+            "amount": 2078.5,
+            "name": "AUTOMATIC PAYMENT - THANK",
+            "category": "GENERAL_MERCHANDISE"
+          },
+          {
+            "amount": 500,
+            "name": "KFC",
+            "category": "FOOD_AND_DRINK"
+          },
+          {
+            "amount": 500,
+            "name": "Madison Bicycle Shop",
+            "category": "GENERAL_MERCHANDISE"
+          }
+        ]
+      },
+      {
+        "date": "2024-07-23",
+        "transactions": [
+          {
+            "amount": 25,
+            "name": "CREDIT CARD 3333 PAYMENT *//",
+            "category": "LOAN_PAYMENTS"
+          },
+          {
+            "amount": 5.4,
+            "name": "Uber 063015 SF**POOL**",
+            "category": "TRANSPORTATION"
+          }
+        ]
+      },
+      {
+        "date": "2024-07-22",
+        "transactions": [
+          {
+            "amount": 5850,
+            "name": "ACH Electronic CreditGUSTO PAY 123456",
+            "category": "GENERAL_SERVICES"
+          },
+          {
+            "amount": 1000,
+            "name": "CD DEPOSIT .INITIAL.",
+            "category": "GENERAL_MERCHANDISE"
+          }
+        ]
+      },
+      {
+        "date": "2024-07-21",
+        "transactions": [
+          {
+            "amount": 78.5,
+            "name": "Touchstone Climbing",
+            "category": "PERSONAL_CARE"
+          },
+          {
+            "amount": -500,
+            "name": "United Airlines",
+            "category": "TRAVEL"
+          }
+        ]
+      },
+      {
+        "date": "2024-07-20",
+        "transactions": [
+          {
+            "amount": 12,
+            "name": "McDonald's",
+            "category": "FOOD_AND_DRINK"
+          },
+          {
+            "amount": 4.33,
+            "name": "Starbucks",
+            "category": "FOOD_AND_DRINK"
+          }
+        ]
+      },
+      {
+        "date": "2024-07-19",
+        "transactions": [
+          {
+            "amount": 89.4,
+            "name": "SparkFun",
+            "category": "GENERAL_MERCHANDISE"
+          }
+        ]
+      },
+      {
+        "date": "2024-07-18",
+        "transactions": [
+          {
+            "amount": -4.22,
+            "name": "INTRST PYMNT",
+            "category": "INCOME"
+          }
+        ]
+      },
+      {
+        "date": "2024-07-08",
+        "transactions": [
+          {
+            "amount": 500,
+            "name": "United Airlines",
+            "category": "TRAVEL"
+          }
+        ]
+      },
+      {
+        "date": "2024-07-06",
+        "transactions": [
+          {
+            "amount": 6.33,
+            "name": "Uber 072515 SF**POOL**",
+            "category": "TRANSPORTATION"
+          }
+        ]
+      },
+      {
+        "date": "2024-07-03",
+        "transactions": [
+          {
+            "amount": 500,
+            "name": "Tectra Inc",
+            "category": "ENTERTAINMENT"
+          }
+        ]
+      },
+      {
+        "date": "2024-07-02",
+        "transactions": [
+          {
+            "amount": 2078.5,
+            "name": "AUTOMATIC PAYMENT - THANK",
+            "category": "GENERAL_MERCHANDISE"
+          },
+          {
+            "amount": 500,
+            "name": "KFC",
+            "category": "FOOD_AND_DRINK"
+          },
+          {
+            "amount": 500,
+            "name": "Madison Bicycle Shop",
+            "category": "GENERAL_MERCHANDISE"
+          }
+        ]
+      },
+      {
+        "date": "2024-06-23",
+        "transactions": [
+          {
+            "amount": 25,
+            "name": "CREDIT CARD 3333 PAYMENT *//",
+            "category": "LOAN_PAYMENTS"
+          },
+          {
+            "amount": 5.4,
+            "name": "Uber 063015 SF**POOL**",
+            "category": "TRANSPORTATION"
+          }
+        ]
+      },
+      {
+        "date": "2024-06-22",
+        "transactions": [
+          {
+            "amount": 5850,
+            "name": "ACH Electronic CreditGUSTO PAY 123456",
+            "category": "GENERAL_SERVICES"
+          },
+          {
+            "amount": 1000,
+            "name": "CD DEPOSIT .INITIAL.",
+            "category": "GENERAL_MERCHANDISE"
+          }
+        ]
+      },
+      {
+        "date": "2024-06-21",
+        "transactions": [
+          {
+            "amount": 78.5,
+            "name": "Touchstone Climbing",
+            "category": "PERSONAL_CARE"
+          },
+          {
+            "amount": -500,
+            "name": "United Airlines",
+            "category": "TRAVEL"
+          }
+        ]
+      },
+      {
+        "date": "2024-06-20",
+        "transactions": [
+          {
+            "amount": 12,
+            "name": "McDonald's",
+            "category": "FOOD_AND_DRINK"
+          },
+          {
+            "amount": 4.33,
+            "name": "Starbucks",
+            "category": "FOOD_AND_DRINK"
+          }
+        ]
+      },
+      {
+        "date": "2024-06-19",
+        "transactions": [
+          {
+            "amount": 89.4,
+            "name": "SparkFun",
+            "category": "ENTERTAINMENT"
+          }
+        ]
+      },
+      {
+        "date": "2024-06-18",
+        "transactions": [
+          {
+            "amount": -4.22,
+            "name": "INTRST PYMNT",
+            "category": "INCOME"
+          }
+        ]
+      },
+      {
+        "date": "2024-06-08",
+        "transactions": [
+          {
+            "amount": 500,
+            "name": "United Airlines",
+            "category": "TRAVEL"
+          }
+        ]
+      },
+      {
+        "date": "2024-06-06",
+        "transactions": [
+          {
+            "amount": 6.33,
+            "name": "Uber 072515 SF**POOL**",
+            "category": "TRANSPORTATION"
+          }
+        ]
+      },
+      {
+        "date": "2024-06-03",
+        "transactions": [
+          {
+            "amount": 500,
+            "name": "Tectra Inc",
+            "category": "ENTERTAINMENT"
+          }
+        ]
+      },
+      {
+        "date": "2024-06-02",
+        "transactions": [
+          {
+            "amount": 2078.5,
+            "name": "AUTOMATIC PAYMENT - THANK",
+            "category": "GENERAL_MERCHANDISE"
+          },
+          {
+            "amount": 500,
+            "name": "KFC",
+            "category": "FOOD_AND_DRINK"
+          },
+          {
+            "amount": 500,
+            "name": "Madison Bicycle Shop",
+            "category": "GENERAL_MERCHANDISE"
+          }
+        ]
+      }
+    ]
+  }
